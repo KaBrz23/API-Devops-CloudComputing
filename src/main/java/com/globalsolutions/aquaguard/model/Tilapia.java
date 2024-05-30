458494d3e2aa9f1697e09195418c485f8be19460
KaBrz23 <kauefernandesbraz@gmail.com>
2024-05-30 16:21:13 -0300

    Atualizando model tilapia

--- src/main/java/com/globalsolutions/aquaguard/model/Tilapia.java
+++ src/main/java/com/globalsolutions/aquaguard/model/Tilapia.java
@@ -3,12 +3,12 @@ package com.globalsolutions.aquaguard.model;
 import java.time.LocalDate;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.globalsolutions.aquaguard.validation.StatusSaude;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Entity
@@ -17,8 +17,8 @@ public class Tilapia {
     @Id @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id_tilapia;
 
-    @StatusSaude
-    private String status_saude;
+    @NotNull(message = "{tilapia.isdoente.notnull}")
+    private boolean isDoente;
 
     @JsonFormat(pattern = "dd/MM/yyyy")
     private LocalDate data;
