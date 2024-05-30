1c8da17631e2417dec5bbd034bd8b03cf3b9228b
KaBrz23 <kauefernandesbraz@gmail.com>
2024-05-30 14:51:31 -0300

    Criar atrubutos da tilapia

--- src/main/java/com/globalsolutions/aquaguard/model/tilapia.java
+++ src/main/java/com/globalsolutions/aquaguard/model/tilapia.java
@@ -1,5 +1,11 @@
 package com.globalsolutions.aquaguard.model;
 
+import java.time.LocalDate;
+
 public class tilapia {
-    
+    private String id_tilapia;
+
+    private String status_saude;
+
+    private LocalDate data;
 }
