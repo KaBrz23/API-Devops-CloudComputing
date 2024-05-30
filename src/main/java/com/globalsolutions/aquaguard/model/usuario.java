386986035abfbbaec49173c9aae56c008fdac443
KaBrz23 <kauefernandesbraz@gmail.com>
2024-05-30 14:39:32 -0300

    Criar atributos para model usuario

--- src/main/java/com/globalsolutions/aquaguard/model/usuario.java
+++ src/main/java/com/globalsolutions/aquaguard/model/usuario.java
@@ -1,5 +1,22 @@
 package com.globalsolutions.aquaguard.model;
 
+import java.time.LocalDate;
+
 public class usuario {
-    
+    private Long id_usuario;
+
+    private String nome;
+
+    private String email;
+
+    private String senha;
+
+    private String telefone;
+
+    private String cpf;
+
+    private LocalDate data_nascimento;
+
+    private String sexo;
+
 }
