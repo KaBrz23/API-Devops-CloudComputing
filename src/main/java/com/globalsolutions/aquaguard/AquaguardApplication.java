d68d73cffa4f9df29be400be0b45ad620e056d09
KaBrz23 <kauefernandesbraz@gmail.com>
2024-05-29 21:39:59 -0300

    Criação do sprint boot

---
+++ src/main/java/com/globalsolutions/aquaguard/AquaguardApplication.java
@@ -0,0 +1,13 @@
+package com.globalsolutions.aquaguard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AquaguardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AquaguardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/globalsolutions/aquaguard/AquaguardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.globalsolutions.aquaguard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AquaguardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
