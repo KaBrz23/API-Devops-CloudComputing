deee0230c4beea9aabcc4db7448d03d7a1f7db6d
KaBrz23 <kauefernandesbraz@gmail.com>
2024-05-30 14:35:33 -0300

    Criar request para a home

--- src/main/java/com/globalsolutions/aquaguard/AquaguardApplication.java
+++ src/main/java/com/globalsolutions/aquaguard/AquaguardApplication.java
@@ -2,12 +2,22 @@ package com.globalsolutions.aquaguard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
+@Controller
 public class AquaguardApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AquaguardApplication.class, args);
+	}
+
+	@RequestMapping
+	@ResponseBody
+	public String home(){
+		return "AquaGuard: Monitoramento Inteligente de Tilápias";
 	}
 
 }
